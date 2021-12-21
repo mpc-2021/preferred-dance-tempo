5e6e910b5644bd6efd087b00984826d057ac87dd
Tommy Rushton <hatchjaw@users.noreply.github.com>
2021-12-21 20:36:01 +0100

    Another config update.

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,7 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter: adapter(),
-		adapter: adapter({ out: 'my-output-directory' }),
+		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
